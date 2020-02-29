6c8e505bf880ac8f29dfc94ed69ec1075dd66ae7
nadeem-nasir <nadeem.nasir@live.com>
2020-02-29 20:57:21 +0100

    Rearange code, code cleanup

--- src/Crayon.Exchangerates.Api/Controllers/ExchangeRatesController.cs
+++ src/Crayon.Exchangerates.Api/Controllers/ExchangeRatesController.cs
@@ -2,6 +2,7 @@
 using Crayon.Exchangerates.Api.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -33,34 +34,41 @@ namespace Crayon.Exchangerates.Api.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetExchangeRates([FromQuery] ExchangeRatesQueryInputModel queryInputModel)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return StatusCode(StatusCodes.Status400BadRequest, ModelState.Values.SelectMany(x => x.Errors));
-            }
-            var inputModel = _helper.Mapper(queryInputModel);
-            if (inputModel.ExchangeRatesDates.Count < 2)
-            {
-                ModelState.AddModelError(nameof(inputModel.ExchangeRatesDates), "ExchangeRatesDates must have at least two date values");
-                return StatusCode(StatusCodes.Status400BadRequest, ModelState.Values.SelectMany(x => x.Errors));
-            }
-
-            inputModel = _helper.OrderInputDates(inputModel);
-
-            var endPointUrl = _helper.GetEndPointUrl("history", inputModel.BaseCurrency, inputModel.TargetCurrency, inputModel.ExchangeRatesDates.First().ExchangeRatesDate, inputModel.ExchangeRatesDates.Last().ExchangeRatesDate);
-            var client = _clientFactory.CreateClient("ExchangeRates");
-            var apiResponse = await client.GetAsync(endPointUrl);
-            if (apiResponse.IsSuccessStatusCode)
-            {
-                var responseString = await apiResponse.Content.ReadAsStringAsync();
-                if (responseString.Length <= 0)
+                if (!ModelState.IsValid)
                 {
-                    return StatusCode(StatusCodes.Status204NoContent);
+                    return StatusCode(StatusCodes.Status400BadRequest, ModelState.Values.SelectMany(x => x.Errors));
                 }
-                var exchangeRatesHistoryResult = _helper.ParseExchangeRatesHistory(responseString, inputModel);
-                var response = _helper.CreateExchangeRatesResponse(exchangeRatesHistoryResult);
-                return StatusCode(StatusCodes.Status200OK, response);
-            } 
-            return StatusCode((int)apiResponse.StatusCode, apiResponse.ReasonPhrase);
+                var inputModel = _helper.Mapper(queryInputModel);
+                if (inputModel.ExchangeRatesDates.Count < 2)
+                {
+                    ModelState.AddModelError(nameof(inputModel.ExchangeRatesDates), "ExchangeRatesDates must have at least two date values");
+                    return StatusCode(StatusCodes.Status400BadRequest, ModelState.Values.SelectMany(x => x.Errors));
+                }
+
+                inputModel = _helper.OrderInputDates(inputModel);
+
+                var endPointUrl = _helper.GetEndPointUrl("history", inputModel.BaseCurrency, inputModel.TargetCurrency, inputModel.ExchangeRatesDates.First().ExchangeRatesDate, inputModel.ExchangeRatesDates.Last().ExchangeRatesDate);
+                var client = _clientFactory.CreateClient("ExchangeRates");
+                var apiResponse = await client.GetAsync(endPointUrl);
+                if (apiResponse.IsSuccessStatusCode)
+                {
+                    var responseString = await apiResponse.Content.ReadAsStringAsync();
+                    if (responseString.Length <= 0)
+                    {
+                        return StatusCode(StatusCodes.Status204NoContent);
+                    }
+                    var exchangeRatesHistoryResult = _helper.ParseExchangeRatesHistory(responseString, inputModel);
+                    var response = _helper.CreateExchangeRatesResponse(exchangeRatesHistoryResult);
+                    return StatusCode(StatusCodes.Status200OK, response);
+                }
+                return StatusCode((int)apiResponse.StatusCode, apiResponse.ReasonPhrase);
+            }
+            catch(Exception ex )
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
